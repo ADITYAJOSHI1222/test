fa4cd07cbf88869e5cc418e146a74a9f14c92d4b
ADITYAJOSHI1222 <91424095+ADITYAJOSHI1222@users.noreply.github.com>
2024-02-01 12:45:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'This is my unthinkable Node!\n'
+  const msg = 'unthinkable Node!\n'
   res.end(msg);
 });
 
